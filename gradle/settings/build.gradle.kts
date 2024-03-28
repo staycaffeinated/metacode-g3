0b119d7ce0de71bfdc221d5e54117d4983f7296f
Jon Caulfield <danesub@yahoo.com>
2024-03-28 04:03:46 -0500

    chore: add gradle settings

---
+++ gradle/settings/build.gradle.kts
@@ -0,0 +1,7 @@
+
+plugins {
+  `kotlin-dsl`
+  }
+
+group = "mmm.coffee.buildlogic"
+
---
+++ gradle/settings/src/main/kotlin/mmm.coffee.repositories.settings.gradle.kts
@@ -0,0 +1,18 @@
+
+pluginManagement {
+  // using 'gradle/plugins' instead of the traditional 'buildSrc'
+  includeBuild("../../gradle/plugins")
+}
+
+dependencyResolutionManagement {
+  repositories.mavenCentral()
+
+  // Platform for dependency versions shared by main build and build-logic
+  includeBuild("../../gradle/platform")
+
+  versionCatalogs.create("libs") {
+    from(files("../../gradle/libs.versions.toml"))
+  }
+
+}
+
