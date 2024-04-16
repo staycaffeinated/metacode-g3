1bfe09013e229c535d5ba61d97cf9e3543fae1fd
Jon Caulfield <danesub@yahoo.com>
2024-04-16 12:14:43 -0500

    build: bump gradle plugins to latest editions

--- gradle/plugins/conventions/build.gradle.kts
+++ gradle/plugins/conventions/build.gradle.kts
@@ -6,12 +6,12 @@ group = "buildlogic"
 
 dependencies {
     // implementation(platform("mmm.coffee.metacode:platform"))
-    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.1.0.3113")
-    implementation("se.solrike.sonarlint:sonarlint-gradle-plugin:1.0.0-beta.15")
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.19.0")
-    implementation("com.palantir.javaformat:gradle-palantir-java-format:2.32.0")
-    implementation("io.freefair.gradle:lombok-plugin:8.0.1")
-    compileOnly("org.projectlombok:lombok:1.18.28")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:5.0.0.4638")
+    implementation("se.solrike.sonarlint:sonarlint-gradle-plugin:2.0.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
+    implementation("com.palantir.javaformat:gradle-palantir-java-format:2.43.0")
+    implementation("io.freefair.gradle:lombok-plugin:8.6")
+    // compileOnly("org.projectlombok:lombok:1.18.28")
 }
 
 
