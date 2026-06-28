e07f74a84e19f4896eb8525d87b5aca653770e3d
jon caulfield <jon.caulfield@example.com>
2026-06-27 20:34:38 -0500

    style: remove unused import

--- mc-common/src/test/java/mmm/coffee/metacode/common/base/PredicateHelperTest.java
+++ mc-common/src/test/java/mmm/coffee/metacode/common/base/PredicateHelperTest.java
@@ -1,6 +1,5 @@
 package mmm.coffee.metacode.common.base;
 
-import org.assertj.core.condition.Not;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
